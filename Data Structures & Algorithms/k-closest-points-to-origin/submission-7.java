7b4c29626eb102d132b9bee19c03c02beb70ff8d
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-03-30 22:34:01 +0200

    Add: k-closest-points-to-origin - submission-7

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1],
+                                                    a[0] * a[0] + a[1] * a[1])
+        );
+
+        for (int[] point : points) {
+            minHeap.offer(point);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[][] res = new int[k][];
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            res[i] = minHeap.poll();
+            i++;
+        }
+
+        return res;
+        
+    }
+}
