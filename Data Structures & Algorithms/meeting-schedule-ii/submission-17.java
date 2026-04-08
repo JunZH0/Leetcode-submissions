60aeffb5da4c6a9a3e0643f255f780fdee76dd26
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-04-08 22:45:45 +0200

    Add: meeting-schedule-ii - submission-17

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-17.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        if (intervals.isEmpty()) {
+            return 0;
+        }
+
+        intervals.sort(Comparator.comparingInt(a -> a.start));
+
+        Queue<Integer> minHeap = new PriorityQueue<>();
+
+        for (Interval interval : intervals) {
+            if (!minHeap.isEmpty() && interval.start >= minHeap.peek()) {
+                minHeap.poll();
+            }
+            minHeap.offer(interval.end);
+        }
+
+        return minHeap.size();
+    }
+}
