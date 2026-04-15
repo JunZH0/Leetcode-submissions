fd0ab81149fdcc4dc3dbb0629871b521012df192
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-04-16 00:38:35 +0200

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    private int ROWS, COLS;
+
+    private int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public void islandsAndTreasure(int[][] grid) {
+        ROWS = grid.length;
+        COLS = grid[0].length;
+
+        Queue<int[]> q = new ArrayDeque<>();
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == 0) {
+                    q.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        if (q.size() == 0) return;
+
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int row = node[0];
+            int col = node[1];
+
+            for (int[] dir : directions) {
+                int nr = row + dir[0];
+                int nc = col + dir[1];
+            
+                if (nr >= 0 && nc >= 0 && nr < ROWS &&
+                    nc < COLS && grid[nr][nc] == Integer.MAX_VALUE) {
+                        q.offer(new int[]{nr, nc});
+                        grid[nr][nc] = grid[row][col] + 1;
+                }
+            }
+        }
+    }
+}
