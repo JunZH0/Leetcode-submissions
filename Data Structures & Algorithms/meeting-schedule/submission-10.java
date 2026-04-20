a787127fd140c53338ef6d6222407f031763eb8e
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-04-20 23:20:53 +0200

    Add: meeting-schedule - submission-10

---
+++ Data Structures & Algorithms/meeting-schedule/submission-10.java
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort(Comparator.comparingInt(a -> a.start));
+
+        for (int i = 1; i < intervals.size(); i++) {
+            int prevEnd = intervals.get(i - 1).end;
+            int currStart = intervals.get(i).start;
+
+            if (currStart < prevEnd) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
