e39e374a3d3dc37a1f4451bb05960510ab08887d
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-04-21 22:59:46 +0200

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    private List<String> res;
+
+    public List<String> generateParenthesis(int n) {
+        res = new ArrayList<>();
+
+        dfs(0, 0, new StringBuilder(), n);
+
+        return res;
+    }
+
+    private void dfs(int openN, int closedN, StringBuilder cur, int n) {
+        if (closedN == openN && openN == n) {
+            res.add(cur.toString());
+            return;
+        }
+
+        if (openN < n) {
+            cur.append('(');
+            dfs(openN + 1, closedN, cur, n);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+
+        if (closedN < openN) {
+            cur.append(')');
+            dfs(openN, closedN + 1, cur, n);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
