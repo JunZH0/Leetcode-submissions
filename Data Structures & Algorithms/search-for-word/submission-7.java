3439bda4568c26df178691c783298cce957c668c
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-05-02 19:23:17 +0200

    Add: search-for-word - submission-7

---
+++ Data Structures & Algorithms/search-for-word/submission-7.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    private int ROWS, COLS;
+
+    public boolean exist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (dfs(r, c, 0, board, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean dfs(int r, int c, int i,char[][] board, String word) {
+        if (i == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || c < 0 || r >= ROWS ||
+            c >= COLS || board[r][c] == '#' ||
+            word.charAt(i) != (board[r][c])) {
+            return false;
+        }
+
+        board[r][c] = '#';
+
+        boolean res = dfs(r + 1, c, i + 1, board, word) ||
+                    dfs(r - 1, c, i + 1, board, word) ||
+                    dfs(r, c + 1, i + 1, board, word) ||
+                    dfs(r, c - 1, i + 1, board, word);
+
+        board[r][c] = word.charAt(i);
+
+        return res;
+    }
+}
