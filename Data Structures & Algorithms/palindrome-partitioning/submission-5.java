3ce82d98e274c4c23c9ecca3766bdc9900c24563
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-05-20 14:24:18 +0200

    Add: palindrome-partitioning - submission-5

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-5.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    List<List<String>> res;
+
+    public List<List<String>> partition(String s) {
+        res = new ArrayList<>();
+
+        backtrack(0, new ArrayList<>(), s);
+
+        return res;
+    }
+
+    private void backtrack(int i, List<String> cur, String s) {
+        if (i >= s.length()) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+    
+
+        for (int j = i; j < s.length(); j++) {
+            if (isPalindrome(i, j, s)) {
+    
+                cur.add(s.substring(i, j + 1));
+                backtrack(j + 1, cur, s);
+                cur.remove(cur.size() - 1);
+            } 
+
+        }
+    }
+
+    private boolean isPalindrome(int l, int r, String s) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
