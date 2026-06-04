271bfd2238d0dab38dd917996ddc904d117a4af3
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2026-06-05 00:19:14 +0200

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for (int num : nums) {
+            int curr = Math.max(num + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+
+    }
+}
